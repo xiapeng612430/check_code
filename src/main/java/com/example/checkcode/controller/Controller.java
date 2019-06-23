d35845263ec9b0c56ed5234ed3f28d4072b47817
xianpeng.xia <951256916>
2019-06-23 09:32:17 +0800

    init

---
+++ src/main/java/com/example/checkcode/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.example.checkcode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-23 09:18
+ */
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping(value = "/index")
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/index");
+        mv.addObject("title", "欢迎使用Thymeleaf!");
+        return mv;
+    }
+}
